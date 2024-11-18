c301ac97fcc697ec07c30566324dd88a2eb83268
jmarualgit <jairelmarual@gmail.com>
2024-11-18 04:21:38 -0800

    add functionality for basic operations +,-,x,/

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
-let number1 = 0;
-let number2 = 0;
-let operation = '';
+let number1 = {data: null, decimalCount: 0};
+let number2 = {data: null, decimalCount: 0};
+let operation = {data: null, locationOfOperationInTextContent: null};
+let answer = null;
+let decimalCount = 0;
 
 document.getElementById("button-clear").addEventListener("click", () => {
     logButtonPressed("CLEAR");
@@ -16,72 +18,77 @@ document.getElementById("button-percent").addEventListener("click", () => {
 
 document.getElementById("button-divide").addEventListener("click", () => {
     logButtonPressed("DIVIDE");
+    setOperator("/");
 });
 
 document.getElementById("button-seven").addEventListener("click", () => {
     logButtonPressed("SEVEN");
-    addNumberToDisplay("7");
+    addToDisplay("7");
 });
 
 document.getElementById("button-eight").addEventListener("click", () => {
     logButtonPressed("EIGHT");
-    addNumberToDisplay("8");
+    addToDisplay("8");
 });
 
 document.getElementById("button-nine").addEventListener("click", () => {
     logButtonPressed("NINE");
-    addNumberToDisplay("9");
+    addToDisplay("9");
 });
 
 document.getElementById("button-multiply").addEventListener("click", () => {
     logButtonPressed("MULTIPLY");
+    setOperator("x");
 });
 
 document.getElementById("button-four").addEventListener("click", () => {
     logButtonPressed("FOUR");
-    addNumberToDisplay("4");
+    addToDisplay("4");
 });
 
 document.getElementById("button-five").addEventListener("click", () => {
     logButtonPressed("FIVE");
-    addNumberToDisplay("5");
+    addToDisplay("5");
 });
 
 document.getElementById("button-six").addEventListener("click", () => {
     logButtonPressed("SIX");
-    addNumberToDisplay("6");
+    addToDisplay("6");
 });
 
 document.getElementById("button-subtract").addEventListener("click", () => {
     logButtonPressed("SUBTRACT");
+    setOperator("-");
 });
 
 document.getElementById("button-one").addEventListener("click", () => {
     logButtonPressed("ONE");
-    addNumberToDisplay("1");
+    addToDisplay("1");
 });
 
 document.getElementById("button-two").addEventListener("click", () => {
     logButtonPressed("TWO");
-    addNumberToDisplay("2");
+    addToDisplay("2");
 });
 
 document.getElementById("button-three").addEventListener("click", () => {
     logButtonPressed("THREE");
-    addNumberToDisplay("3");
+    addToDisplay(3);
 });
 
 document.getElementById("button-plus").addEventListener("click", () => {
     logButtonPressed("PLUS");
+    setOperator("+");
 });
 
 document.getElementById("button-zero").addEventListener("click", () => {
     logButtonPressed("ZERO");
-    addNumberToDisplay("0");
+    addToDisplay("0");
 });
 
 document.getElementById("button-modulus").addEventListener("click", () => {
     logButtonPressed("MODULUS");
+    setOperator("MOD");
 });
 
 document.getElementById("button-decimal").addEventListener("click", () => {
@@ -90,13 +97,66 @@ document.getElementById("button-decimal").addEventListener("click", () => {
 
 document.getElementById("button-equal").addEventListener("click", () => {
     logButtonPressed("EQUAL");
+    getAnswer();
 });
-
 
 function logButtonPressed(buttonName) {
     console.log(`Button Pressed: ${buttonName}`);
 }
 
-function addNumberToDisplay(charInput) {
+function addToDisplay(charInput) {
     document.getElementById("textInputArea").textContent = document.getElementById("textInputArea").textContent + `${charInput}`;
+}
+
+function setOperator(operatorInput) {
+    if (operation.data == null) {
+
+        operation.data = operatorInput;
+        setFirstNumber();
+    
+        document.getElementById("textInputArea").textContent = document.getElementById("textInputArea").textContent + ` ${operatorInput} `;
+
+        const text = document.getElementById("textInputArea").textContent;
+        operation.locationOfOperationInTextContent = text.indexOf(operatorInput);
+
+        console.log("current location of operator is " + index);
+    } else {
+        setFirstNumber();
+    }
+}
+
+function setFirstNumber() {
+    if (number1.data == null) {
+        number1.data = Number(document.getElementById("textInputArea").textContent);
+    }
+
+    console.log("number1.data is " + number1.data);
+}
+
+function getAnswer() {
+    const fullText = document.getElementById("textInputArea").textContent
+    const textAfterOperator = fullText.substring((operation.locationOfOperationInTextContent + 2));
+
+    number2.data = Number(textAfterOperator);
+    console.log("number2.data: " + number2.data);
+
+    switch(operation.data) {
+        case "+":
+            answer = number1.data + number2.data;
+            break;
+        
+        case "-":
+            answer = number1.data - number2.data;
+            break;
+
+        case "x":
+            answer = number1.data * number2.data;
+            break;
+
+        case "/":
+            answer = number1.data / number2.data;
+            break;
+    }
+
+    document.getElementById("textInputArea").textContent = document.getElementById("textInputArea").textContent + " = " + answer;
 }
